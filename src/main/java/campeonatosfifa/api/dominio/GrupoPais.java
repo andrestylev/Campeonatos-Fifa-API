5a95fc10780992a526226843cb7ffd84024d23af
Andres <asqg14@gmail.com>
2025-11-11 16:34:38 -0500

    comienzo

---
+++ src/main/java/campeonatosfifa/api/dominio/GrupoPais.java
@@ -0,0 +1,11 @@
+package campeonatosfifa.api.dominio;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "GrupoPais")
+public class GrupoPais {
+
+     
+}
